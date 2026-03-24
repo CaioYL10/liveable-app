be63ca7f97e0216e75deb8bafca8aaaf52584f39
CaioYL10 <caioyl15.ask@gmail.com>
2026-03-24 18:58:44 -0300

    feat(ui): add card-em-alta component

---
+++ liveable-frontend/src/modules/properties/components/Card-em-alta.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+    import { PhFire, PhArrowRight } from "@phosphor-icons/vue";
+</script>
+
+<template>
+    <div class="all">
+        <div class="informs">
+            <div class="informs-texts">
+                <h3>Casa de frente ao mar</h3>
+                <p>R$300,00 p/ noites ★ 5</p>
+                <p>
+                    Mais de 70 mil cliques!
+                    <PhFire :size="20" />
+                </p>
+            </div>
+
+            <button>
+                <PhArrowRight :size="32" />
+            </button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .all {
+        width: 25vw;
+        min-height: 350px;
+        background-color: purple;
+        border-radius: 22px;
+        display: flex;
+        align-items: flex-end;
+    }
+
+    .informs {
+        width: 100%;
+        min-height: 100px;
+        background-color: rgb(12, 34, 236);
+        display: flex;
+        justify-content: space-between;
+        padding: 0 20px;
+    }
+</style>
--- liveable-frontend/src/modules/properties/views/propertyDetails.vue
+++ liveable-frontend/src/modules/properties/views/propertyDetails.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
     import Exib from '@/modules/properties/components/TheExib.vue';
     import Editor from '@/modules/properties/components/TheEditor.vue';
+    import CardEmAlta from '../components/card-em-alta.vue';
 </script>
 
 <template>
-    <Exib/>
-    <Editor/>
+    <CardEmAlta />
 </template>
