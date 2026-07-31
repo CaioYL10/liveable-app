a5b0687ad37f13a470572dae5332a716e7ab147a
CaioYL10 <caioyl15.ask@gmail.com>
2026-07-31 14:40:23 -0300

    refactoring entrypoint.sh

--- liveable-backend/entrypoint.sh
+++ liveable-backend/entrypoint.sh
@@ -8,8 +8,8 @@ echo "Limpando caches de config/route..."
 php artisan config:clear
 php artisan route:clear
 
-echo "Rodando migrations (opcional - remova se não quiser migrar automaticamente)..."
-# php artisan migrate --force
+echo "Rodando migrations..."
+php artisan migrate --force
 
 echo "Iniciando servidor..."
 php artisan serve --host=0.0.0.0 --port=10000
